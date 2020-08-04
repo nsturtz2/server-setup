859116ea88aaf1900b8a8fa3d57ceacd9f596099
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-04 15:59:53 -0500

    Create netdata.sh

---
+++ netdata.sh
@@ -0,0 +1,2 @@
+bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+sudo netdata-claim.sh -token=jt6k10P6ThinNGRRYlptRqS0HJfYMzdmWHOM5dtCM-kpSGHpXbY0LqVc7cvNB8o_IIMIbuLUI2fEfyIjiNT5TCXYWHKCKRCUEEWiTPOCMS5k01YooH2JDinOxnL2sUvxsQuB9yo -rooms=560f7e85-08ad-4303-887f-d5bd433ec6b7 -url=https://app.netdata.cloud
