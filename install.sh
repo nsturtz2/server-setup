02031d67acc49969920790c45d44eb150892c90f
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-06 14:25:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir /NSCS
 apt install bind9 php apache2 -y 
 apt install mariadb-server libapache2-mod-php7.4 -y
 apt -y install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl
-apt -y install php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip
+apt -y install php7.4-gmp php7.4-bcmath php7.4-imagick php7.4-xml php7.4-zip
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install tasksel -y
 cd /var/www/
@@ -17,3 +17,4 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo apt-get update -y
 sudo apt-get install certbot python3-certbot-apache python3
+reboot
