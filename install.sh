89d152137e32c8a6bd735ec375836973806e28ad
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-06 10:59:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #! /bin/bash
 mkdir /NSCS
-cd /NSCS
-apt install apache2 mariadb-server libapache2-mod-php7.4 -y
+apt install bind9 php apache2 -y 
+apt install mariadb-server libapache2-mod-php7.4 -y
 apt -y install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl
 apt -y install php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip
-mkidr cron
-cd cron
-mkdir cron.d cron.w cron.h cron.m
-cd ..
-mkdir installer
-cd installer
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install lightdm
-wget https://raw.githubusercontent.com/Sturtz-Network/server-setup/master/netdata.sh
-chmod 777 *
-./netdata.sh
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install tasksel -y
+cd /var/www/
+wget https://download.nextcloud.com/server/releases/nextcloud-19.0.1.zip
+apt install unzip tree mlocate -y
+unzip nextcloud-19.0.1.zip
+chown -R www-data:www-data /var/www/nextcloud
+chown -R www-data:www-data /data
