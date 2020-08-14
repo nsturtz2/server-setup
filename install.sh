f5d40d2a7ebd48b218f6bd13ce48eaeb0eec5b30
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-14 09:30:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 mkdir /NSCS
-apt install bind9 php apache2 -y 
-apt install mariadb-server libapache2-mod-php7.4 -y
+cd /NSCS
+sudo hostnamectl set-hostname mail.sturtz.ml
+sudo nano /etc/hosts
+sudo apt install git -y
+cd iRedMail/
+chmod +x iRedMail.sh
+sudo bash iRedMail.sh
+git clone https://github.com/iredmail/iRedMail.git
+apt install bind9 php -y
 apt -y install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl
 apt -y install php7.4-gmp php7.4-bcmath php7.4-imagick php7.4-xml php7.4-zip
 sudo apt-get update -y && sudo apt-get upgrade -y
@@ -16,5 +23,5 @@ sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo apt-get update -y
-sudo apt-get install certbot python3-certbot-apache python3 -y 
+sudo apt-get install certbot python3-certbot-nginx python3 -y 
 reboot
