5d537f429eaeed32ef42a86e9d98a2d598dbb948
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-24 09:58:22 -0500

    Create nextcloud.sql

---
+++ nextcloud.sql
@@ -0,0 +1,4 @@
+CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'username'@'localhost';
+FLUSH PRIVILEGES;
