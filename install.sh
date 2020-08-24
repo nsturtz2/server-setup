6d099fa083fc0944cb0cb37679bb0cd9208baf16
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-24 10:35:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl 
 sudo apt install php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip -y
 wget https://raw.githubusercontent.com/Sturtz-Network/server-setup/master/nextcloud.sql
 mysql < nextcloud.sql
+rm nextcloud.sql
 cd /var/www/
 wget https://download.nextcloud.com/server/releases/nextcloud-19.0.1.zip
 unzip nextcloud-19.0.1.zip
@@ -19,11 +20,24 @@ sudo netdata-claim.sh -token=GArtxDhzl_0ZY-1M36s_NJaKSQbMZC8yfgYoF6LxYQhfGVPmNji
 sudo chown -R www-data:www-data /var/www/nextcloud/
 sudo -u www-data php occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "username" --database-pass "password" --admin-user "admin" --admin-pass "password" --data-dir="/data"
 sudo snap install --classic certbot
-cd /NSCS
-rm nextcloud.sql
 echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -y
 sudo apt update -y
 sudo apt install webmin -y
+cd /NSCS
+mkdir scripts cron
+cd scripts
+touch dir.sh status.sh mysql.sh apache.sh php-packs.sh email.sh
+cd /NSCS/cron
+mkdir cron.d cron.w cron.h cron.m
+cd /NSCS/cron/cron.d
+touch cron
+cd /NSCS/cron/cron.w
+touch cron
+cd /NSCS/cron/cron.h
+touch cron
+cd /NSCS/cron/cron.m
+touch cron
+chown -R +X /NSCS
 wall Done!
 wall Done Script!
