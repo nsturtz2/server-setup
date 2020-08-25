a19677f58aa72773d69785a3dd2134e5bccf2bca
Sturtz Network <67589701+nsturtz@users.noreply.github.com>
2020-08-25 07:25:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,18 @@ rm nextcloud.sql
 cd /var/www/
 wget https://download.nextcloud.com/server/releases/nextcloud-19.0.1.zip
 unzip nextcloud-19.0.1.zip
-chown -R www-data:www-data *
+rm -r nextcloud-19.0.1.zip html
+chown -R www-data:www-data nextcloud
+cd nextcloud
+sudo -u www-data php /var/www/nextcloud/occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "username" --database-pass "password" --admin-user "admin" --admin-pass "password" --data-dir="/data"
 cd /
 chown -R www-data:www-data data
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --stable-channel --disable-telemetry
 sudo netdata-claim.sh -token=GArtxDhzl_0ZY-1M36s_NJaKSQbMZC8yfgYoF6LxYQhfGVPmNjiUht8ur_3GqOd8XN1hZonSju6wVvCCd7Kz_LZK2-ZrtS3uIJwtV1NvhdI_FzNDo4olpghK-v9emiM07z9nRqU -rooms=074f1e95-65d9-47ed-b4ff-345316b28bd4 -url=https://app.netdata.cloud
-sudo chown -R www-data:www-data /var/www/nextcloud/
-sudo -u www-data php occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "username" --database-pass "password" --admin-user "admin" --admin-pass "password" --data-dir="/data"
+sudo chown -R www-data:www-data /data
 sudo snap install --classic certbot
 echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list
-wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -y
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add 
 sudo apt update -y
 sudo apt install webmin -y
 cd /NSCS
@@ -38,6 +40,6 @@ cd /NSCS/cron/cron.h
 touch cron
 cd /NSCS/cron/cron.m
 touch cron
-chown -R +X /NSCS
+chmod -R +X /NSCS
 wall Done!
 wall Done Script!
